7657ca69cc2a2c840b5b13fa34cae7a9dd632740
TUCchkul <42653001+TUCchkul@users.noreply.github.com>
2022-05-30 14:40:09 +0200

    update init_set.sh

--- init_setup.sh
+++ init_setup.sh
@@ -1,4 +1,5 @@
 conda create --prefix ./env python=3.7 -y
 source activate ./env
-pip3 install torch torchvision torchaudio
+#pip3 install torch torchvision torchaudio
 pip install -r requirements.txt
+conda env export > conda.yaml
